194bf53f86615e23f4db6e1abb97e6a3dc6c4918
Jamil-4khtar <jamil4khtar@gmail.com>
2024-09-08 13:03:29 +0530

    button default prevented

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-let s_name = document.queryselectorAll("input");
+let s_name = document.querySelector("#name").value;
+let s_year = document.querySelector("#year").value;
+let s_btn = document.querySelector("button");
+
+s_btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    
+    let url = document.getElementById("url");
+    // console.log(url);
+    url.innerHTML = 
+    
+    
+    
+})
+
