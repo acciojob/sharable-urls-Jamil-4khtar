b90222f6dbbfbabdf7bcd81e677499a8c8cc35fe
Jamil-4khtar <jamil4khtar@gmail.com>
2024-09-08 14:07:47 +0530

    Assignment Khatam

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
-let s_name = document.querySelector("#name").value;
-let s_year = document.querySelector("#year").value;
 let s_btn = document.querySelector("button");
 
 s_btn.addEventListener("click", (e) => {
     e.preventDefault();
     
-    let url = document.getElementById("url");
+    let s_name = document.querySelector("#name").value;
+    let s_year = document.querySelector("#year").value;
+
     // console.log(url);
-    url.innerHTML = 
+    let queryPara = new URLSearchParams();
+    queryPara.set('name', s_name);
+    queryPara.set('year', s_year);
     
+    let url = document.getElementById("url");
     
-    
+    // console.log(queryPara);
+
+    url.textContent += queryPara.toString();
 })
 
